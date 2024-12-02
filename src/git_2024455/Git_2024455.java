4f7d4a8f738f15fe8d053e62419d8bf440638c96
Kml251 <2024455@student.cct.ie>
2024-12-02 20:10:31 +0000

    New project to test git

---
+++ src/git_2024455/Git_2024455.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024455;
+
+/**
+ *
+ * @author kamil
+ */
+public class Git_2024455 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This is a test project for conneting to GitHub.
+        
+        System.out.println("Kamil 2024455");
+    }
+    
+}
